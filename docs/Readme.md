173d0388359d94d15d3cbe745b9918ccf2dfd4cf
hyeon002 <hhj48770@naver.com>
2024-03-18 10:07:18 +0900

    추가

---
+++ docs/Readme.md
@@ -0,0 +1,4 @@
+**java script**
+
+**change**
+- change 이벤트 는 사용자가 요소의 값을 변경할 때 발생함 
--- src/index.js
+++ src/index.js
@@ -4,49 +4,43 @@ const buttons = document.querySelectorAll(".button");
 const customInput = document.querySelector('.select-input');
 const showTip = document.querySelector(".display-amount-tip");
 const showTotal = document.querySelector('.display-amount-total');
+const reset = document.querySelector('.container-button');
+const inputs = document.getElementsByTagName('input');
 
-let billTotal = 0, people;
-let tipAmount, total;
-
+let billTotal = 0;
+let people = 0;
+let tipAmount = 0;
+let total = 0;
 
 billInput.addEventListener("change", function() {
-  billTotal = Number(billInput);
-  // calculate();
+  billTotal = parseFloat(billInput);
 })
 peopleInput.addEventListener("change", function() {
-  people = Number(peopleInput.value);
-  // calculate();
+  people = parseFloat(peopleInput.value);
+  output();
 })
 
 let percent = 0;
 
-buttons.forEach(function(button) {
-  button.addEventListener("click", function() {
-    percent = Number(button);
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function() {
+    percent = parseFloat(btn.textContent);
   })
 })
 
-// function calculate() {
-//   if (billTotal != 0 && people != 0 && percent != 0){
-//     tipAmount = (billTotal * (percent/100)) / people;
-//     total = (billTotal + tipAmount) / people;
-//     showTip.innerHTML = "$" + tipAmount.toFixed(2);
-//     showTotal.innerHTML = "$" + total.toFixed(2);
-//   }
-// }
+function output() {
+  console.log(billTotal);
+  console.log(people);
+  console.log(percent);
+  tipAmount = (billTotal * (percent/100)) / people;
+  total = (billTotal + tipAmount) / people;
+  showTip.innerHTML = "$" + tipAmount.toFixed(2);
+  showTotal.innerHTML = "$" + total.toFixed(2);
+}
 
-document.querySelectorAll("input").forEach(function (input) {
-  input.addEventListener("change", function() {
-    billTotal = Number(billInput);
-    people = Number(peopleInput.value);
-
-    if (percent != 0) {
-      tipAmount = (billTotal * (percent/100)) / people;
-      total = (billTotal + tipAmount) / people;
-
-      showTip.textContent = "$" + tipAmount.toFixed(2);
-      showTotal.textContent = "$" + total.toFixed(2);
-
-    }
-  })
+reset.addEventListener("click", function() {
+  billInput.value = '';
+  peopleInput.value = '';
+  showTip.innerHTML = "$0.00";
+  showTotal.innerHTML = "$0.00";
 })
